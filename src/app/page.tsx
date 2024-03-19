7bc8b1a906d78754266f5973b9239c58e3fbff7d
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-03-20 02:17:42 +0900

    add : panda css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { css } from "../../styled-system/css";
+
 export default function Home() {
-	return <main></main>;
+	return (
+		<main>
+			<div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
+				Hello 🐼!
+			</div>
+		</main>
+	);
 }
