ee01bf095a29c832d0a82d942cc4d33ee0dcf35b
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-03-20 03:12:35 +0900

    move :file

--- src/app/quickstart/page.tsx
+++ src/app/quickstart/page.tsx
@@ -1,3 +1,17 @@
+import AvatarView from "@/components/qucikstart/AvatarView";
+import Counters from "@/components/qucikstart/Counters";
+import ListItems from "@/components/qucikstart/ListItems";
+import { Stack } from "../../../styled-system/jsx";
+
 export default function Home() {
-	return <main></main>;
+	return (
+		<main>
+			{" "}
+			<Stack>
+				<Counters />
+				<AvatarView />
+				<ListItems />
+			</Stack>
+		</main>
+	);
 }
