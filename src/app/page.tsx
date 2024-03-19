ff4669fa784daf1cd9cfbb833196606944df43a2
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-03-20 02:36:46 +0900

    add : avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
+import MyButton from "@/ui/MyButton";
+import Image from "next/image";
 import { css } from "../../styled-system/css";
+
+const user = {
+	name: "Hedy Lamarr",
+	imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+	imageSize: 90,
+};
 
 export default function Home() {
 	return (
 		<main>
 			<div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
-				Hello 🐼!
+				Welcome to my app
 			</div>
+			<MyButton />
+			<h1>{user.name}</h1>
+			<Image
+				className="avatar"
+				src={user.imageUrl}
+				alt={"Photo of " + user.name}
+				width={user.imageSize}
+				height={user.imageSize}
+			/>
 		</main>
 	);
 }
