358b0f9ef89eeac8abc717ff715107e28f0e9333
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-03-20 14:27:35 +0900

    refactor : remove Useless state

--- src/ui/tic-tac-toe/Game.tsx
+++ src/ui/tic-tac-toe/Game.tsx
@@ -8,21 +8,19 @@ import Board from "./Board";
 export type Squares = string | null;
 
 const Game = () => {
-	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
 	const [currentMove, setCurrentMove] = useState(0);
 	const currentSquares = history[currentMove];
+	const xIsNext = currentMove % 2 === 0;
 
 	const handlePlay = (nextSquares: Squares[]) => {
 		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
 		setHistory(nextHistory);
 		setCurrentMove(nextHistory.length - 1);
-		setXIsNext(!xIsNext);
 	};
 
 	const jumpTo = (nextMove: number) => {
 		setCurrentMove(nextMove);
-		setXIsNext(nextMove % 2 === 0);
 	};
 
 	const moves = history.map((squares, move) => {
