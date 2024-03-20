64bff6ce45211b373d864c14e8e1381150b02d57
satohaCC <un.cappuccinooo.per.favore+github@gmail.com>
2024-03-20 14:31:03 +0900

    refactor : 関数を分離

--- src/ui/tic-tac-toe/Board.tsx
+++ src/ui/tic-tac-toe/Board.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { Grid } from "../../../styled-system/jsx";
-import { Squares } from "./Game";
+
 import Square from "./Square";
+import { Squares, calculateWinner } from "./libs";
 
 type Props = {
 	xIsNext: boolean;
@@ -48,23 +49,3 @@ const Board = ({ xIsNext, squares, onPlay }: Props) => {
 };
 
 export default Board;
-
-const calculateWinner = (squares: Squares[]) => {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
-		}
-	}
-	return null;
-};
